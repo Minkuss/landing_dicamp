73541e87f4fa506a258f2dfbb6f7e0d08c3ed10d
HonFi <kasanindaniil@gmail.com>
2023-10-06 15:34:44 +1000

    +Footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,46 @@
+import { FC } from "react";
+import Logo from "../../assets/logo.png";
+import { Link, NavLink } from "react-router-dom";
+import "./Footer.scss";
+
+export const Footer: FC = () => {
+    return (
+        <div className="dicamp-footer">
+            <ul className="dicamp-footer_ul">
+                <li>
+                   <img 
+                   src={Logo} alt="Логотоп" 
+                   className="dicamp-footer_ul_img"/>
+                </li>
+                <li>
+                    <span className="dicamp-footer_ul_span">
+                        Цифровой кампус
+                    </span>
+                </li>
+                <div className="dicamp-footer_ul_div">
+                <li>
+                    <NavLink to={"/"} className="dicamp-footer_ul_link">
+                        <span>
+                            Пользовательское соглашение
+                        </span>
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to={"/"} className="dicamp-footer_ul_link">
+                        <span>
+                            Политика конфеденциальности
+                        </span>
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to={"/"} className="dicamp-footer_ul_link">
+                        <span>
+                            Удаление персональных данных
+                        </span>
+                    </NavLink>
+                </li>
+                </div>
+            </ul>
+        </div>
+    )
+}
--- src/pages/StudentsPage/StudentsPage.tsx
+++ src/pages/StudentsPage/StudentsPage.tsx
@@ -2,12 +2,14 @@ import { FC } from "react";
 import { Link } from "react-router-dom";
 import { Header } from "../../components/Header/Header";
 import { ThirdMission } from "../../components/ThirdMission/ThirdMission";
+import { Footer } from "../../components/Footer/Footer";
 
 export const StudentsPage: FC = () => {
   return (
     <div>
       <Header />
       <ThirdMission />
+      <Footer />
     </div>
   );
 };
