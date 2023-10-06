02d9cffa7be4fc92b9f1a7fdb71cb624ef8ac279
Никита Шиян <nikusia0804@gmail.com>
2023-10-06 17:53:39 +1000

    +sendrequest

--- src/components/SendRequest/SendRequest.tsx
+++ src/components/SendRequest/SendRequest.tsx
@@ -1,15 +1,40 @@
 import { FC } from "react";
 import "./SendRequest.scss";
+import convert from "../../assets/convert.png";
 
 export const SendRequest: FC = () => {
   return (
     <div className="sending-req">
       <div className="sending-req_info">
         <p className="sending-req_info_p">
-          <span>Подайте </span>
-          <span>заявку </span>
-          <span>и с вами свяжутся!</span>
+          <div className="sending-req_info_p_span1">Подайте </div>
+          <div className="sending-req_info_p_span2">заявку</div>
+          <div>и с вами свяжутся!</div>
         </p>
+        <p className="sending-req_info_p2">
+          Разместите ваши промо-материалы, вакансии, опросы на постоянно
+          растущую аудиторию студентов
+        </p>
+        <img className="sending-req_info_img" src={convert} alt="Конверт" />
+      </div>
+      <div className="sending-req_form">
+        <input className="sending-req_form_inp" type="text" />
+        <input className="sending-req_form_inp" type="text" />
+        <input className="sending-req_form_inp" type="text" />
+        <textarea className="sending-req_form_text"></textarea>
+        <input
+          className="sending-req_form_check"
+          type="checkbox"
+          name=""
+          id=""
+        />
+        <input
+          className="sending-req_form_check"
+          type="checkbox"
+          name=""
+          id=""
+        />
+        <button>Отправить заявку</button>
       </div>
     </div>
   );
