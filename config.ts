cfe8f60cf373c056f502e36a28d425523d712c09
Никита Шиян <nikusia0804@gmail.com>
2023-10-09 11:45:51 +1000

    обновление gitingnore для того, чтобы всё работало

---
+++ config.ts
@@ -0,0 +1,20 @@
+export const apiDvgups = [
+  "https://festu.dicamp.ru/api/auth-service/statistics/by-types?types=TOTAL_USERS",
+  "https://festu.dicamp.ru/api/event-service/statistics/by-types?types=TOTAL_EVENTS",
+  "https://festu.dicamp.ru/api/event-service/statistics/by-types?types=TOTAL_BONUS_VALUE",
+  "https://festu.dicamp.ru/api/order-service/statistics/by-types?types=TOTAL_BONUSES_SPENT"
+]
+
+export const apiTogu = [
+  "https://pnu.dicamp.ru/api/auth-service/statistics/by-types?types=TOTAL_USERS",
+  "https://pnu.dicamp.ru/api/event-service/statistics/by-types?types=TOTAL_EVENTS",
+  "https://pnu.dicamp.ru/api/event-service/statistics/by-types?types=TOTAL_BONUS_VALUE",
+  "https://pnu.dicamp.ru/api/order-service/statistics/by-types?types=TOTAL_BONUSES_SPENT"
+]
+
+export const apiAnother = [
+  "https://campus.dicamp.ru/api/auth-service/statistics/by-types?types=TOTAL_USERS",
+  "https://campus.dicamp.ru/api/event-service/statistics/by-types?types=TOTAL_EVENTS",
+  "https://campus.dicamp.ru/api/event-service/statistics/by-types?types=TOTAL_BONUS_VALUE",
+  "https://campus.dicamp.ru/api/order-service/statistics/by-types?types=TOTAL_BONUSES_SPENT"
+]
